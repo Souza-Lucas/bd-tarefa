93f1e6a23ddb1813f3d26a58442d652284b0a1de
Lucas Vinícius Oliveira Souza <77822244+Souza-Lucas@users.noreply.github.com>
2022-05-13 17:16:07 -0300

    Update tarefa01-q08.sql

--- tarefas/t01/tarefa01-q08.sql
+++ tarefas/t01/tarefa01-q08.sql
@@ -1,1 +1,5 @@
+CREATE VIEW depto_projetos(gerente, descricao, projetos)
+as SELECT departamento.cod_gerente, departamento.descricao, count(projeto.cod_depto)
+FROM projeto p, departamento d WHERE p.cod_depto = d.codigo GROUP BY d.descricao, d.cod_gerente;
 
+SELECT f.nome, descricao, projeto FROM depto_projetos d LEFT JOIN funcionario f ON f.codigo = d.gerente;
