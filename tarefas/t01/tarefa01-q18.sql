8592e4b5dfb1391096c64dfb127220ef894cdad9
Lucas Vinícius Oliveira Souza <77822244+Souza-Lucas@users.noreply.github.com>
2022-05-13 17:36:26 -0300

    Update tarefa01-q18.sql

--- tarefas/t01/tarefa01-q18.sql
+++ tarefas/t01/tarefa01-q18.sql
@@ -1,1 +1,7 @@
-
+SELECT funcionario.nome , departamento.descricao FROM funcionario, departamento
+WHERE funcionario.salario IN(SELECT MAX(funcionario.salario)
+				  FROM funcionario LEFT JOIN departamento
+				  ON funcionario.cod_depto = departamento.codigo GROUP BY departamento.codigo
+) AND funcionario.cod_depto = departamento.codigo
+GROUP BY funcionario.salario, funcionario.nome, departamento.descricao
+ORDER BY funcionario.salario;
