134025f461ba998e0141b2d19afc3cd573b80d20
Lucas Vinícius Oliveira Souza <77822244+Souza-Lucas@users.noreply.github.com>
2022-05-13 17:32:47 -0300

    Update tarefa01-q14.sql

--- tarefas/t01/tarefa01-q14.sql
+++ tarefas/t01/tarefa01-q14.sql
@@ -1,1 +1,5 @@
+CREATE VIEW atv_funcionarios(nome_funcionario, quantidade_atvs) AS SELECT funcionario.nome, count(atividade.cod_responsavel) 
+FROM funcionario, atividade
+WHERE atividade.cod_responsavel = funcionario.codigo GROUP BY funcionario.nome;
 
+SELECT nome_funcionario, quantidade_atvs FROM atv_funcionarios
