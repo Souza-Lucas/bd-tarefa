3fe03f04ea5f9adc5b7ec74f0c125926a59c22aa
Lucas Vinícius Oliveira Souza <77822244+Souza-Lucas@users.noreply.github.com>
2022-05-13 16:05:22 -0300

    Update tarefa01-create.sql

--- tarefas/t01/tarefa01-create.sql
+++ tarefas/t01/tarefa01-create.sql
@@ -1,51 +1,54 @@
-CREATE TABLE funcionario(
-codigo int AUTO_INCREMENT,
+CREATE TABLE funcionario (
+	codigo serial,
 	nome varchar(50),
 	sexo char(1),
-	dtNasc date,
+	dt_nasc date,
 	salario decimal(10,2),
-	codDepto int,
+	cod_depto int,
 	PRIMARY KEY (codigo),
 	FOREIGN KEY (codSupervisor) REFERENCES funcionario(codigo) on delete set null on update cascade
 );
 
-CREATE TABLE departamento(
-  	codigo int AUTO_INCREMENT,
+CREATE TABLE departamento (
+	codigo serial,
 	descricao varchar(50),
-	codGerente int,
+	cod_gerente int,
 	PRIMARY KEY (codigo),
 	UNIQUE KEY depSigla (sigla),
 	FOREIGN KEY (codGerente) REFERENCES funcionario(codigo) on delete set null on update cascade
 );
 
-
-CREATE TABLE projeto(
-  	codigo int AUTO_INCREMENT,
+CREATE TABLE projeto (
+	codigo serial,
 	nome varchar(50),
 	descricao varchar(250),
-	codResponsavel int,
-	codDepto int,
-	dataInicio date, 
-	dataFim date,
+	cod_responsavel int,
+	cod_depto int,
+	data_inicio date, 
+	data_fim date,
 	PRIMARY KEY (codigo),
 	UNIQUE KEY projNome (nome),
 	FOREIGN KEY (codResponsavel) REFERENCES funcionario(codigo) on delete set null on update cascade,
 	FOREIGN KEY (codDepto) REFERENCES departamento(codigo) on delete set null on update cascade
 );
-
-CREATE TABLE atividade(
-  	codigo int AUTO_INCREMENT,
-  	nome varchar(50),
+ 
+CREATE TABLE atividade (
+	codigo serial,
+	nome varchar(50),
 	descricao varchar(250),
-	codProjeto int,
-	dataInicio date, 
-	dataFim date,
+	cod_responsavel int,
+	data_inicio date, 
+	data_fim date,
 	PRIMARY KEY (codigo),
 	FOREIGN KEY (codProjeto) REFERENCES projeto(codigo) on delete set null on update cascade
 
 );
 
-CREATE TABLE atividade_projeto(
+CREATE TABLE atividade_projeto (
 	cod_projeto int,
-	cod_atividade int
+ 	cod_atividade int,
+	FOREIGN KEY (cod_projeto) REFERENCES projeto(codigo) on delete set null on update cascade,
+  	FOREIGN KEY (cod_atividade) REFERENCES atividade(codigo) on delete set null on update cascade
+
 );
+
