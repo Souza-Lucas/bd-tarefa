3dcf07ab5d20f419f4ed6cd106b61be3a3a6ed92
Lucas Vinícius Oliveira Souza <77822244+Souza-Lucas@users.noreply.github.com>
2022-05-13 17:25:39 -0300

    Update tarefa01-q12.sql

--- tarefas/t01/tarefa01-q12.sql
+++ tarefas/t01/tarefa01-q12.sql
@@ -1,1 +1,2 @@
-
+SELECT projeto.nome, projeto.data_inicio, projeto.data_fim, atividade.descricao, atividade.data_inicio, atividade.data_fim FROM projeto, atividade, atividade_projeto
+WHERE projeto.codigo = atividade_projeto.cod_projeto AND atividade.codigo = atividade_projeto.cod_projeto;
